80e2a1894e389fb7488d9a63258041956b1d01f4
Max <dacostaareal@hotmail.com>
2026-08-12 14:53:48 +0100

    Migration 12-08-2026 git

--- docs/js/map.js
+++ docs/js/map.js
@@ -29,7 +29,7 @@ async function chargerEtapes(){
 
     ETAPES = await fetch("data/index.json")
         .then(response => response.json());
-	0afficherStatsVoyage();
+	afficherStatsVoyage();
     await afficherListeEtapes();
 
 }
